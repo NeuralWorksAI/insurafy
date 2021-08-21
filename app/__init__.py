940fa5d0c3460954c70bbd4266ceb25497a17566
00MB <s2061204@ed.ac.uk>
2021-08-21 15:41:45 +0100

    sass

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
+import sass
 
 app = Flask(__name__)
+
+sass.compile(dirname=('./app/static/scss', './app/static/css'))
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,14 @@
 from app import app
-
+from flask import render_template, request, redirect, url_for, flash, jsonify
+from os import getenv
+from pymongo import MongoClient
+from dotenv import load_dotenv
+load_dotenv()
 
 @app.route('/')
 def index():
-    return 'Hello ou!'
+    return render_template("index.html")
+
+@app.route('/signin')
+def signin():
+    return render_template("signin.html")
