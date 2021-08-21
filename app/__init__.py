744ce5248acd8537b5f70bb76a97a681e9637db3
00MB <s2061204@ed.ac.uk>
2021-08-21 19:45:19 +0100

    index and stuff

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from sassutils.wsgi import SassMiddleware
 import sass
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "key"
+
+app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    'app': ('static/scss', 'static/css', '/static/css')
+})
 
 sass.compile(dirname=('./app/static/scss', './app/static/css'))
 
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from app import app
+from app.forms import LoginForm, RegisterForm
 from flask import render_template, request, redirect, url_for, flash, jsonify
 from os import getenv
 from pymongo import MongoClient
@@ -9,6 +10,23 @@ load_dotenv()
 def index():
     return render_template("index.html")
 
-@app.route('/signin')
+@app.route('/signin', methods=['GET', 'POST'])
 def signin():
-    return render_template("signin.html")
+    form=LoginForm()
+    if form.validate_on_submit():
+        print("valid")
+        return redirect(url_for('dashboard'))
+    else:
+        print("invalid")
+    return render_template("signin.html", form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form=RegisterForm()
+    if form.validate_on_submit():
+        return redirect(url_for('dashboard'))
+    return render_template("signup.html", form=form)
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
